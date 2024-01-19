c7bae34172213646b07bcab0ea31cfb1c3877b43
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-01-19 12:15:39 +0100

    Create ShoppingList.js Affichage de la liste des catégories et plantes disponibles Données dynamiques depuis src/datas/plantList.js

---
+++ src/components/ShoppingList.js
@@ -0,0 +1,27 @@
+import { plantList } from "../datas/plantList";
+
+const plantCategories = plantList.reduce((categories, plant) => {
+  if (!categories.includes(plant.category)) {
+    categories.push(plant.category);
+  }
+  return categories;
+}, []);
+
+function ShoppingList() {
+  return (
+    <div>
+      <ul className="lms-categories">
+        {plantCategories.map((category, index) => (
+          <li key={`${category}-${index}`}>{category}</li>
+        ))}
+      </ul>
+      <ul className="lms-plants">
+        {plantList.map((plant) => (
+          <li key={plant["id"]}>{plant["name"]}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ShoppingList;
